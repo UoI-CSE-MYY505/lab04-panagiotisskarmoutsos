695862e8ff0756bd51ed6a4cf7464093682d7643
panagiotisskarmoutsos <114870585+panagiotisskarmoutsos@users.noreply.github.com>
2024-11-09 03:05:55 +0200

    Add files via upload

--- lab04.s
+++ lab04.s
@@ -30,8 +30,22 @@ str_ge:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
-            jr   ra
- 
+lw t0 0(a0)
+lw t1 4(a0)
+whileloop:
+    bne t0, zero, iflabel
+    addi a0, a0, 4
+    lw t0 0(a0)
+    lw t1 4(a0)
+    j whileloop
+iflabel:
+    blt t0, t1, returnzero
+    addi a0, zero, 1
+    j out
+returnzero:
+    add a0, zero, zero
+out:
+            jr   ra 
 # ----------------------------------------------------------------------------
 # recCheck(array, size)
 # if size == 0 or size == 1
@@ -46,4 +60,15 @@ recCheck:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
+
+beq a0, zero, out2
+addi t3, zero, 1
+beq a0, t3, out2
+bne a0, t3, end
+addi a0, a0, 4
+addi t3, a0, 4
+j recCheck
+out2:
+j str_ge
+end:
             jr   ra
